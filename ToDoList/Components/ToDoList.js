d9e8fbc6829581043802b03cb136395cde6b249f
naamir21 <nida.aamir@edu.sait.ca>
2024-10-31 12:48:35 -0600

    Lab 3 - Props and State Management

--- ToDoList/Components/ToDoList.js
+++ ToDoList/Components/ToDoList.js
@@ -1,29 +1,19 @@
 import { View, Text, StyleSheet, ScrollView, Pressable } from 'react-native';
 import React from 'react';
 
-const ToDoList = () => {
+function ToDoList({ tasks }) {
   return (
-    <View>
-      <ScrollView>
-        <Pressable>
-          <View style={[styles.task, styles.completed]}>
-            <Text style={styles.taskText}>Do laundry</Text>
-          </View>
-        </Pressable>
-        <Pressable>
-          <View style={[styles.task]}>
-            <Text style={styles.taskText}>Go to gym</Text>
-          </View>
-        </Pressable>
-        <Pressable>
-          <View style={[styles.task, styles.completed]}>
-            <Text style={styles.taskText}>Walk dog</Text>
-          </View>
-        </Pressable>
-      </ScrollView>
-    </View>
+    <ScrollView>
+        {tasks.map((task, index) => (
+          <Pressable key={index}>
+            <View style={styles.task}>
+              <Text style={styles.taskText}>{task}</Text>
+            </View>
+          </Pressable>
+        ))}
+    </ScrollView>
   );
-};
+}
 
 export default ToDoList;
 
--- ToDoList/app/index.js
+++ ToDoList/app/index.js
@@ -1,11 +1,20 @@
 import { StyleSheet, Text, View } from "react-native";
 import ToDoForm from "../Components/ToDoForm";
 import ToDoList from "../Components/ToDoList";
+import { useState } from "react";
 
 export default function Page() {
+  const [tasks, setTasks] = useState(
+    [
+      'Do laundry', 
+      'Go to gym', 
+      'Walk dog'
+    ]
+  );
+
   return (
     <View style={styles.container}>
-      <ToDoList/>
+      <ToDoList tasks={tasks}/>
       <ToDoForm/>
     </View>
   );
